8b644166cc83a4d4cec5e5da629b1b28b7608155
BebertDeChast <74964221+BebertDeChast@users.noreply.github.com>
2021-04-29 14:56:09 +0200

    pep8 is cool

--- db_functions.py
+++ db_functions.py
@@ -4,19 +4,17 @@ import sqlite3
 connection = sqlite3.connect('./bdd.db')
 
 
-def ajout_client(nom, prenom) :
-
+def ajout_client(nom: str, prenom: str, date_de_naissance=None, mdp: str = '1234', mail: str = 'unknown', pays: str = 'France', ville: str = 'Marseille', adresse: str = '48 rue de la boustifaille', cp: int = 10251) -> None:
     """ Rajoute dans la BD un client avec le nom et prénom fournis, les autres attributs auront des arguments par défaut """
 
     connection.execute(f"""
     INSERT INTO client
     (NOM_CLIENT,PRENOM_CLIENT,DATE_NAISS_client,hash_mdp_client,mail_client,pays_client,ville_client,adresse_client,cp_client)
-    VALUES ('{nom}', '{prenom}', 'Inconnu', 1234, 'Inconnu', 'Inconnu', 'Inconnu', 'Inconnu', 'Inconnu')
+    VALUES ('{nom}', '{prenom}', '{date_de_naissance}', '{mdp}', '{mail}', '{pays}', '{ville}', '{adresse}', '{cp}')
     """)
 
 
-def affiche_smartphone(marque):
-
+def affiche_smartphone(marque: str) -> None:
     """ Affiche la table obtenue, attention l'argument est sensible aux majuscules"""
 
     afficher_table(f"""
@@ -27,14 +25,14 @@ def affiche_smartphone(marque):
 
 
 # Petite fonction bonus car nous on aime automatiser
-def afficher_table(query):
+def afficher_table(query: str) -> None:
     """ Prend en entré une requête SQL (SELECT statement) et l'affiche """
     for row in connection.execute(query):
-        print(row) # Testé et approuvé par la street (moi)
+        print(row)  # Testé et approuvé par la street (moi)
+
 
 # Un petit test
 affiche_smartphone('Huawei')
-
 
 # Sauvegarde les modifications à la BD et coupe la connexion
 connection.commit()
